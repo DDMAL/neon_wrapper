1658069f89c67ddbb63d6f0cb87fb97ffeb03d9b
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-05-27 11:27:33 -0400

    Update to use modular Neon

--- editor.js
+++ editor.js
@@ -1,9 +1,27 @@
+import DisplayPanel from './Neon2/src/DisplayPanel/DisplayPanel.js';
+import SingleView from './Neon2/src/SingleView/SingleView.js';
+import SingleEditMode from './Neon2/src/SingleEdit/SingleEditMode.js';
+import InfoModule from './Neon2/src/InfoModule.js';
 import NeonView from './Neon2/src/NeonView.js';
 
-var view = new NeonView({
-    meifile: meiFile,
-    bgimg: bgImg,
-    mode: "rodan",
-    raw: rawData
+fetch(new Request(meiFile)).then(response => {
+  if (response.ok) {
+    let map = new Map();
+    response.text().then(data => {
+      map.set(0, data);
+      var view = new NeonView({
+          mode: 'single',
+          options: {
+            image: bgImg,
+            name: 'Rodan Correction',
+            meiMap: map
+          },
+          View: SingleView,
+          Display: DisplayPanel,
+          Info: InfoModule,
+          Edit: SingleEditMode
+      });
+      view.start();
+    });
+  }
 });
-view.start();
