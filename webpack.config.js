311cbbdf0bf120e11ee5e0011b4ea6d0c1ef671d
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-08-02 08:53:49 -0400

    Update constants

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,8 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            LINK_LOCATION: JSON.stringify('#')
+            __LINK_LOCATION__: JSON.stringify('#'),
+            __NEON_VERSION__: JSON.stringify('v4.0.0')
         })
     ]
 };
