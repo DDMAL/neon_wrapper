ed88c627c78d90a768233b0dbbe9e5e0f680d0c6
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-01-20 15:44:18 -0500

    Conform with new structure used in Neon

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,10 @@ let commitHash = childProcess.execSync('(cd Neon && git rev-parse --short HEAD)'
 module.exports = {
     mode: "production",
     entry: {
-        editor: "./editor.js",
-        pretty: "./Neon/src/pretty.ts"
+        editor: "./editor.ts",
     },
     output: {
-        path: path.resolve(__dirname, "static"),
+        path: path.resolve(__dirname, "static", "Neon"),
         filename: "[name].js"
     },
     node: {
@@ -27,40 +26,11 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: "./img/"
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.rng$/,
-                use: [
-                    'raw-loader'
-                ]
-            },
-            {
                 test: /Worker\.js/,
                 use: [
-                    'worker-loader'
-                ]
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    'html-loader'
+                    {
+                      loader: 'worker-loader',
+                    }
                 ]
             }
         ]
@@ -75,7 +45,8 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             __LINK_LOCATION__: JSON.stringify('#'),
-            __NEON_VERSION__: JSON.stringify('Commit ' + commitHash)
+            __NEON_VERSION__: JSON.stringify('Commit ' + commitHash),
+            __ASSET_PREFIX__: JSON.stringify('')
         })
     ]
 };
